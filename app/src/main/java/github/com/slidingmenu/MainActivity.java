4dc9248ce9434d807999ce95408c7648ef470f11
sk <sk@github.com>
2017-05-23 13:58:40 +0800

    screenshot

--- app/src/main/java/github/com/slidingmenu/MainActivity.java
+++ app/src/main/java/github/com/slidingmenu/MainActivity.java
@@ -2,6 +2,9 @@ package github.com.slidingmenu;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
+
+import github.com.slidingmenu.view.SlidingMenu;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,23 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+//        SlidingMenu mSlidingMenu= (SlidingMenu) findViewById(R.id.sliding_menu);
+//        //切换菜单状态
+//        mSlidingMenu.toggleMenu();
+//        //监听菜单状态
+//        mSlidingMenu.setOnSlidingMenuStatusChangeListenter(new SlidingMenu.OnSlidingMenuStatusChangeListenter() {
+//            @Override
+//            public void close() {
+//                Toast.makeText(MainActivity.this,"菜单关闭",Toast.LENGTH_SHORT).show();
+//            }
+//
+//            @Override
+//            public void open() {
+//                Toast.makeText(MainActivity.this,"菜单打开",Toast.LENGTH_SHORT).show();
+//            }
+//        });
+
+
     }
 }
